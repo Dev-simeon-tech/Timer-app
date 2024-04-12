a5a67fff849dff9a255293294dbc48bff9303cbf
Dev simeon <elebesunuosereme@gmail.com>
2024-04-12 20:55:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('.start');
+const pauseButton = document.querySelector('.pause');
+const circle = document.querySelector('#circle')
+
+const circleRadius = circle.getAttribute('r');
+const perimeter = 2 * circleRadius * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration = 0;
+const timer = new Timer(durationInput,startButton,pauseButton,
+   {
+    onStart(totalDuration) {
+        duration = totalDuration;
+
+    },
+    onTick(timeRemaning) {
+        circle.setAttribute('stroke-dashoffset',
+        (perimeter * timeRemaning)/duration - perimeter); 
+        // this math equation above will return the offset value for each tick      
+    
+    },
+    onComplete() {
+        console.log('complete')
+    },
+   });
+
+// console.log(t1)
---
+++ timer.js
@@ -0,0 +1,64 @@
+class Timer{
+    constructor(durationInput, startButton, pauseButton,callbacks){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        if(callbacks){
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+        
+        this.startButton.addEventListener('click',this.start)
+        this.pauseButton.addEventListener('click',this.pause)
+    }
+    start = () =>{
+        if(this.onStart){
+            this.onStart(this.timeRemaning);
+        }
+        this.tick();
+        this.disableInput();
+        this.startButton.setAttribute('disabled',true);
+        this.times = setInterval(this.tick,50);
+    }
+    pause = () =>{
+    
+        clearInterval(this.times);  
+        this.startButton.removeAttribute('disabled');
+        this.enableInput();
+    }
+    tick = () =>{
+
+        if(this.timeRemaning <= 0){
+            // this function stops the timer when it reaches 0
+            this.pause(); 
+            if(this.onComplete){
+                this.onComplete();
+                this.enableInput();     
+            }
+        }
+        else{
+            // this.timeRemaning = this.timeRemaning - 1;
+            this.timeRemaning = this.timeRemaning - 0.05;
+            if(this.onTick){
+                this.onTick(this.timeRemaning);
+            }
+        }
+        // const timeRemaning = this.timeRemaning;
+    }
+    
+    get timeRemaning(){
+        return parseFloat(this.durationInput.value)
+    }
+    set timeRemaning(time){
+        this.durationInput.value = time.toFixed(2);
+    }
+    disableInput = () =>{
+        this.durationInput.setAttribute('disabled',true)
+    }
+    enableInput = () =>{
+        this.durationInput.removeAttribute('disabled')
+    }
+   
+}
